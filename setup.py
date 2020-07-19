2f91bdb7ef6c4c60923cdec14e6c95c49ed471d2
Vyacheslav Klyuchnikov <vk@chaika.com>
2020-07-19 23:42:05 +0300

    fix in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='alpha-compiler-vk',
-      version='0.1a',
-      description='Python tools for quantitative finance',
-      author='Peter Harrington',
-      author_email='peter.b.harrington@gmail.com',
-      maintainer='Vyacheslav Klyuchnikov',
-      url='https://www.alphacompiler.com/',
+      version='0.1.1',
+      description='Python tools for quantitative finance (originally by Peter Harrington, peter.b.harrington@gmail.com)',
+      author='Vyacheslav Klyuchnikov',
+      author_email='vyacheslav.klyuchnikov@gmail.com',
+      url='https://github.com/vk643/alpha-compiler-vk/',
       packages=['alphacompiler',
           'alphacompiler.util', 
           'alphacompiler.data', 
